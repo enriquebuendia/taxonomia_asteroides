c65544e0c61cc7811d356720ff668c9868ee4355
Enrique Buendia <enriquebuendiav@gmail.com>
2026-03-24 10:49:01 -0600

    Update main.py to create a summary using archivos.py

--- main.py
+++ main.py
@@ -17,10 +17,10 @@ my_parser.add_argument('txt',
                        metavar='txt',
                        type=str,
                        help='Archivo que contiene los datos del espectro a analizar, la primera columna debe contener la longitud de onda, y la segunda el flujo normalizado')
-my_parser.add_argument("--smooth", 
+my_parser.add_argument("--smooth",
                        help="Bandera para activar el suavizado mediante un spline3",
                     action="store_true")
-my_parser.add_argument("--micras", 
+my_parser.add_argument("--micras",
                        help="Si la longitud de onda de su archivo esta en en micras, activar esta bandera",
                     action="store_true")
 args = my_parser.parse_args()
@@ -31,7 +31,7 @@ script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
 abs_file_path = os.path.join(script_dir, 'espectro_txt/' + archivo)
 while not os.path.isfile(abs_file_path):
     print('El archivo no existe')
-    sys.exit()  
+    sys.exit()
 txt = open(abs_file_path,'r')
 espectro_txt = pd.read_csv(txt,header=None,delim_whitespace=True) ### El archivo no debe tener encabezados, y el delimitador debe se espacio en blanco
 txt.close()
@@ -51,28 +51,30 @@ proba=np.asarray(proba)
 proba=proba.reshape(3,5)
 #%%Crear tabla (DataFrame)
 d = {' ':range(1,11,1),
-     'Bus-DeMeo':demeo_tax[1], 
-     'Bus & Binzel':bus_tax[1][:10], 
+     'Bus-DeMeo':demeo_tax[1],
+     'Bus & Binzel':bus_tax[1][:10],
      'Dist_euc':bus_tax[0][:10]}
 tabla = pd.DataFrame(data = d)
 #%% Imprimir en terminal
 print('')
 print(tabulate(tabla, headers=['Bus - DeMeo','Bus & Binzel', 'Dist Euc (B&B)'], showindex=False))
 taxos_list=[]
-for n in [10]:
+# Calculamos taxonomía propuesta para distintos k (10, 30, 50) para el archivo resumen
+for n in [10, 30, 50]:
     taxo_prop, pesos = tools.datos(bus_tax[1], n)
-    taxos_list.append(tools.datos(bus_tax[1], n))
-    peso_mayor=pesos[np.argmax(pesos)]/np.sum(pesos)
+    taxos_list.append((taxo_prop, pesos))
+
+    peso_mayor = pesos[np.argmax(pesos)]/np.sum(pesos)
     print(taxo_prop, pesos/np.sum(pesos))
-    #print(taxos_prop[1]/np.sum(taxos_prop[1]))
     print("\n Taxonomía propuesta para k = {}: {} con función de peso de {:2f}".format(n, taxo_prop[np.argmax(pesos)], peso_mayor))
+
 print('\nProbabilidades de tipos para el KNN original')
 #%%Crear tabla (DataFrame)
 #probas=np.array(proba,dtype=[('S','<i4'),('C','<i4'),('X','<i4'),('PS','<i4'),('NS','<i4')])
 filas= np.array([0,1,2], dtype=np.intp)
-#print(proba[filas,[0,0,0]])
-d = {'Tipo S':proba[filas,[0,0,0]], 
-     'Tipo C':proba[filas,[1,1,1]], 
+# print(proba[filas,[0,0,0]])
+d = {'Tipo S':proba[filas,[0,0,0]],
+     'Tipo C':proba[filas,[1,1,1]],
      'Tipo X':proba[filas,[2,2,2]],
      'Positive Slope':proba[filas,[3,3,3]],
      'Negative Slope':proba[filas,[4,4,4]]}
@@ -83,8 +85,17 @@ print(tabulate(tablaKNN, headers=['Tipo S','Tipo C', 'Tipo X', 'PS', 'NS'], show
 predicKNN=np.array([np.argmax(proba[0]),np.max(proba[0])])
 predicPCA=np.array([np.argmax(proba[1]),np.max(proba[1])])
 predicNCA=np.array([np.argmax(proba[2]),np.max(proba[2])])
-#print(predicPCA[0],predicPCA[1])
 #%% Escribir txt resumen
-#archivos.crea_res(archivo, taxos_list[-1],predicKNN,predicPCA,predicNCA)
+# archivos.crea_res espera una lista FP con 3 entradas (k=10,30,50), cada una como (taxonomia, fp)
+FP = []
+for (taxo_prop, pesos) in taxos_list:
+    fp = pesos[np.argmax(pesos)]/np.sum(pesos)
+    FP.append((taxo_prop[np.argmax(pesos)], fp))
+
+# Asegurar que exista el directorio de salida
+os.makedirs(os.path.join(script_dir, 'output'), exist_ok=True)
+
+# Crear resumen en output/summary_<archivo>
+archivos.crea_res(archivo, FP, predicKNN, predicPCA, predicNCA)
+
 tools.graficar(suavizado,newLO,int(archivo[1:7]))
- 
